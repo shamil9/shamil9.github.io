8835caf472646f6a18ab8ff40d8de6bf10e7ca26
shamil9 <shamil9@gmail.com>
2017-07-03 11:47:25 +0200

    Updated infos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '5::'
+const VERSION = '6::';
 const ASSETS = [
     '/assets/css/style-1.css',
     '/assets/img/icons/envelope.svg',
@@ -6,32 +6,30 @@ const ASSETS = [
     '/assets/img/icons/github.svg',
     '/assets/img/icons/t.svg',
     '/assets/img/logo.svg',
-    '/assets/img/nfss.png',
+    '/assets/img/visualisr.png',
     '/assets/img/prolocks.png',
     '/assets/js/app.js',
     '/assets/js/particles.min.js',
     '/docs/AltamirovShamil_eng.pdf',
     '/assets/js/particles.json'
-]
+];
 
-self.addEventListener( 'install', e => {
+self.addEventListener('install', e => {
     e.waitUntil(
-        caches.open( VERSION + 'shamilbe' ).then( cache => {
-            return cache.addAll( ASSETS )
-                .then( () => self.skipWaiting() )
-        } )
-    )
-} )
+        caches.open(VERSION + 'shamilbe').then(cache => {
+            return cache.addAll(ASSETS).then(() => self.skipWaiting());
+        })
+    );
+});
 
-self.addEventListener( 'activate', event => {
-    event.waitUntil( self.clients.claim() )
-} )
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
 
-self.addEventListener( 'fetch', event => {
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match( event.request )
-        .then( response => {
-            return response || fetch( event.request );
-        } )
-    )
-} )
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
